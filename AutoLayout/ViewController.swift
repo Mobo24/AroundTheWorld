53694663310b974539336f476a5010e7ff04c7c8
Mobo <princemoronfolu@gmail.com>
2020-03-22 12:27:22 -0500

    Initial Commit

---
+++ AutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Mobolaji Moronfolu on 3/22/20.
+//  Copyright © 2020 Mobolaji Moronfolu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
